52fbf0d8d6603bdbd72a7e894281efb681bda500
Oluwashola <abdulafeezoluwashola2@gmail.com>
2026-01-21 18:12:55 +0100

    service type

---
+++ data/services.ts
@@ -0,0 +1,28 @@
+import { Service } from "@/types/services"
+
+export const SERVICES_DATA: Service[] = [
+  {
+    title: "Professional Project Management",
+    description: "Expert oversight ensuring timely and quality-driven project completion.",
+    image: "/Image/Card.png",
+    variant: "large"
+  },
+  {
+    title: "Affordable Housing Solutions",
+    description: "Innovative housing options designed to meet diverse budget requirements.",
+    image: "/Image/Card2.png",
+    variant: "small"
+  },
+  {
+    title: "Verified Land Acquisition",
+    description: "Secure and legally verified land procurement for your dream project.",
+    image: "/Image/Card3.png",
+    variant: "small"
+  }
+]
+
+export const SERVICES_CONTENT = {
+  heading: "Our Approach",
+  title: "Comprehensive Construction Solutions",
+  subtitle: "Delivering end-to-end services that meets your unique real estate needs."
+}
---
+++ types/services.ts
@@ -0,0 +1,13 @@
+export type ServiceCardProps = {
+  title: string
+  description: string
+  image: string
+  variant?: 'large' | 'small'
+}
+
+export type Service = {
+  title: string
+  description: string
+  image: string
+  variant: 'large' | 'small'
+}
