0969e6d13ce7f5cd938f7fe642168d5895c8a2bd
Oluwashola <abdulafeezoluwashola2@gmail.com>
2026-01-23 00:07:44 +0100

    image

---
+++ types/team.ts
@@ -0,0 +1,22 @@
+export type TeamMember = {
+  name: string
+  role: string
+  image: string
+  instagram?: string
+  twitter?: string
+  linkedin?: string
+}
+
+export type FollowJourneyData = {
+  title: string
+  backgroundImage: string
+  instagram: string
+  twitter: string
+  linkedin: string
+}
+
+export type TeamSectionData = {
+  title: string
+  subtitle: string
+  members: TeamMember[]
+}
